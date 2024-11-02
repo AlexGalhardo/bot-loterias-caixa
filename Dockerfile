2f6206cdcf372220e957f5a64372728d352d20ae
Alex Galhardo <aleexgvieira@gmail.com>
2024-11-02 12:32:19 -0300

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,70 +1,55 @@
-# Use a slim version of Node.js for the build stage
 FROM node:20.11.1-slim AS builder
 
-# Set the working directory
+# Install necessary packages for Firefox
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    firefox-esr \
+    ca-certificates \
+    fonts-liberation \
+    libasound2 \
+    libdbus-glib-1-2 \
+    libgtk-3-0 \
+    libx11-xcb1 \
+    libxtst6 \
+    xauth \
+    xvfb \
+    && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /usr/www/bot-loteria
 
-# Set environment variables
-ENV SERVER_PORT=3000 \
-    NODE_ENV=local \
-    LOTERIAS_CAIXA_LOTOMANIA_URL=https://loterias.caixa.gov.br/Paginas/Lotomania.aspx \
-    GOOGLE_SEARCH_LOTOMANIA_NUMBERS_URL=https://www.google.com/search?q=caixa+lotomania \
-    HEADLESS=true \
-    CPF=45672888828 \
-    PASSWORD=15915915 \
-    LOCALE_DATE_TIME=pt-BR \
-    BROWSER_PATH="/usr/bin/google-chrome" \
-    ENABLE_TELEGRAM_LOGS=true \
-    TELEGRAM_BOT_HTTP_TOKEN=7648290812:AAGAZFunY3wZy3qcOiZNu46P6jNPninvLKQ \
-    TELEGRAM_BOT_CHANNEL_ID=1477312913 \
-    GOOGLE_CLIENT_ID=780167494697-u4ca3hibrer9cim6b1lg8c61k03gcah1.apps.googleusercontent.com \
-    GOOGLE_CLIENT_SECRET=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 \
-    START_BOT_FIRST_WEEK_DATE=1 \
-    START_BOT_SECOND_WEEK_DATE=3 \
-    START_BOT_THIRD_WEEK_DATE=5 \
-    START_BOT_HOUR=11 \
-    START_BOT_MINUTE=49 \
-    VERIFY_LOTOMANIA_FIRST_WEEK_DATE=1 \
-    VERIFY_LOTOMANIA_SECOND_WEEK_DATE=3 \
-    VERIFY_LOTOMANIA_THIRD_WEEK_DATE=5 \
-    VERIFY_LOTOMANIA_HOUR=21 \
-    VERIFY_LOTOMANIA_MINUTE=0
+# Set up environment variables to inherit values from the host
+ENV SERVER_PORT \
+    NODE_ENV \
+    LOTERIAS_CAIXA_LOTOMANIA_URL \
+    GOOGLE_SEARCH_LOTOMANIA_NUMBERS_URL \
+    HEADLESS \
+    CPF \
+    PASSWORD \
+    LOCALE_DATE_TIME \
+    BROWSER_PATH="/usr/bin/firefox" \
+    ENABLE_TELEGRAM_LOGS \
+    TELEGRAM_BOT_HTTP_TOKEN \
+    TELEGRAM_BOT_CHANNEL_ID \
+    GOOGLE_CLIENT_ID \
+    GOOGLE_CLIENT_SECRET \
+    START_BOT_FIRST_WEEK_DATE \
+    START_BOT_SECOND_WEEK_DATE \
+    START_BOT_THIRD_WEEK_DATE \
+    START_BOT_HOUR \
+    START_BOT_MINUTE \
+    VERIFY_LOTOMANIA_FIRST_WEEK_DATE \
+    VERIFY_LOTOMANIA_SECOND_WEEK_DATE \
+    VERIFY_LOTOMANIA_THIRD_WEEK_DATE \
+    VERIFY_LOTOMANIA_HOUR \
+    VERIFY_LOTOMANIA_MINUTE
 
-# Copy package.json and install dependencies
 COPY package.json ./
 RUN npm install
 
-# Install Google Chrome
-RUN apt-get update && \
-    apt-get install -y wget gnupg && \
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/googlechrome-linux-keyring.gpg && \
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrome-linux-keyring.gpg] https://dl-ssl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list && \
-    apt-get update && \
-    apt-get install -y google-chrome-stable --no-install-recommends && \
-    rm -rf /var/lib/apt/lists/*
-
-# Copy the rest of the application code
 COPY . .
 
-# Build the application
 RUN npm run build
 
-# Create a new stage for production
-FROM node:20.11.1-slim AS production
-
-# Set the working directory for the production stage
-WORKDIR /usr/www/bot-loteria
-
-# Copy built files and node_modules from the builder stage
-COPY --from=builder /usr/www/bot-loteria/dist ./dist
-COPY --from=builder /usr/www/bot-loteria/node_modules ./node_modules
-COPY --from=builder /usr/www/bot-loteria/package*.json ./
-
-# Install production dependencies only
-RUN npm install --only=production
-
-# Expose the application port
 EXPOSE 3000
 
-# Start the application
 CMD ["npm", "run", "start:prod"]
--- src/lotomania/start-bot.ts
+++ src/lotomania/start-bot.ts
@@ -8,211 +8,211 @@ import { randomUUID } from "node:crypto";
 import DateTime from "@/utils/date-time.util";
 
 const readJsonFile = (filePath) => {
-    const data = readFileSync(filePath, "utf-8");
-    return JSON.parse(data);
+	const data = readFileSync(filePath, "utf-8");
+	return JSON.parse(data);
 };
 
 const writeJsonFile = (filePath, data) => {
-    writeFileSync(filePath, JSON.stringify(data, null, 2));
+	writeFileSync(filePath, JSON.stringify(data, null, 2));
 };
 
 export async function startBot() {
-    const options = new firefox.Options();
-    options.addArguments('--headless'); // Enable headless mode for Firefox
-    options.addArguments("--no-sandbox"); // Recommended for headless mode
-    options.addArguments("--disable-dev-shm-usage"); // Recommended for headless mode
+	const options = new firefox.Options();
+	options.addArguments("--headless"); // Enable headless mode for Firefox
+	options.addArguments("--no-sandbox"); // Recommended for headless mode
+	options.addArguments("--disable-dev-shm-usage"); // Recommended for headless mode
 
-    const driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
+	const driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
 
-    try {
-        console.log(`\n\nStarting Lottery Bot...\n\n`);
+	try {
+		console.log(`\n\nStarting Lottery Bot...\n\n`);
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        // console.log('driver -> ', driver)
+		// console.log('driver -> ', driver)
 
-        await driver.get(process.env.LOTERIAS_CAIXA_LOTOMANIA_URL);
-        console.log(`Step 1 -> Navigated to ${process.env.LOTERIAS_CAIXA_LOTOMANIA_URL}`);
+		await driver.get(process.env.LOTERIAS_CAIXA_LOTOMANIA_URL);
+		console.log(`Step 1 -> Navigated to ${process.env.LOTERIAS_CAIXA_LOTOMANIA_URL}`);
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        // console.log('driver -> ', driver)
+		// console.log('driver -> ', driver)
 
-        await driver.findElement(By.css(".linkAposta a")).click();
-        console.log("Step 2 -> Clicked 'Aposte agora'");
+		await driver.findElement(By.css(".linkAposta a")).click();
+		console.log("Step 2 -> Clicked 'Aposte agora'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("botaosim")).click();
-        console.log("Step 3 -> Clicked 'Sim'");
+		await driver.findElement(By.id("botaosim")).click();
+		console.log("Step 3 -> Clicked 'Sim'");
 
-        // await driver.wait(until.elementLocated(By.id("botaosim")), 10000);
-        // const button = await driver.wait(until.elementIsVisible(driver.findElement(By.id("botaosim"))), 10000); // Ensures it's visible
-        // await driver.wait(until.elementIsEnabled(button), 10000); // Ensures the button is enabled
-        // await button.click();
-        // console.log("Step 3 -> Clicked 'Sim'");
+		// await driver.wait(until.elementLocated(By.id("botaosim")), 10000);
+		// const button = await driver.wait(until.elementIsVisible(driver.findElement(By.id("botaosim"))), 10000); // Ensures it's visible
+		// await driver.wait(until.elementIsEnabled(button), 10000); // Ensures the button is enabled
+		// await button.click();
+		// console.log("Step 3 -> Clicked 'Sim'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("btnLogin")).click();
-        console.log("Step 4 -> Clicked 'Login'");
+		await driver.findElement(By.id("btnLogin")).click();
+		console.log("Step 4 -> Clicked 'Login'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("username")).sendKeys(process.env.CPF);
-        console.log("Step 5 -> Entered CPF");
+		await driver.findElement(By.id("username")).sendKeys(process.env.CPF);
+		console.log("Step 5 -> Entered CPF");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("button-submit")).click();
-        console.log("Step 6 -> Clicked 'Próximo'");
+		await driver.findElement(By.id("button-submit")).click();
+		console.log("Step 6 -> Clicked 'Próximo'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.css("button[name='login']")).click();
-        console.log("Step 7 -> Clicked 'Receber código'");
+		await driver.findElement(By.css("button[name='login']")).click();
+		console.log("Step 7 -> Clicked 'Receber código'");
 
-        await driver.sleep(5000);
+		await driver.sleep(5000);
 
-        const activationCode = await getActivationCodeFromGmail();
-        TelegramLog.info(`Activation code received: ${activationCode}`);
-        await driver.findElement(By.id("codigo")).sendKeys(activationCode);
-        console.log(`Step 8 -> Entered activation code`);
+		const activationCode = await getActivationCodeFromGmail();
+		TelegramLog.info(`Activation code received: ${activationCode}`);
+		await driver.findElement(By.id("codigo")).sendKeys(activationCode);
+		console.log(`Step 8 -> Entered activation code`);
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.css("button[name='login'][onclick='return validDV()']")).click();
-        console.log("Step 9 -> Clicked 'Login'");
+		await driver.findElement(By.css("button[name='login'][onclick='return validDV()']")).click();
+		console.log("Step 9 -> Clicked 'Login'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("password")).sendKeys(process.env.PASSWORD);
-        console.log("Step 10 -> Entered password");
+		await driver.findElement(By.id("password")).sendKeys(process.env.PASSWORD);
+		console.log("Step 10 -> Entered password");
 
-        await driver.sleep(3000);
+		await driver.sleep(3000);
 
-        const entrarButton = await driver.findElement(By.xpath("//button[contains(text(), 'Entrar')]"));
-        await entrarButton.click();
-        console.log("Step 11 -> Clicked 'Entrar'");
+		const entrarButton = await driver.findElement(By.xpath("//button[contains(text(), 'Entrar')]"));
+		await entrarButton.click();
+		console.log("Step 11 -> Clicked 'Entrar'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.css("li .data-jogo-menu-lotomania")).click();
-        console.log("Step 12 -> Clicked 'Lotomania'");
+		await driver.findElement(By.css("li .data-jogo-menu-lotomania")).click();
+		console.log("Step 12 -> Clicked 'Lotomania'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("adopt-accept-all-button")).click();
-        console.log("Step 12.1 -> Aceitando a PORRA do cookie");
+		await driver.findElement(By.id("adopt-accept-all-button")).click();
+		console.log("Step 12.1 -> Aceitando a PORRA do cookie");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        let gamesToMade = 0;
-        const HOW_MUCH_GAMES_TO_PLAY = 7;
+		let gamesToMade = 0;
+		const HOW_MUCH_GAMES_TO_PLAY = 7;
 
-        console.log("Step 13 -> Creating 7 games...");
+		console.log("Step 13 -> Creating 7 games...");
 
-        while (gamesToMade < HOW_MUCH_GAMES_TO_PLAY) {
-            await driver.findElement(By.id("completeojogo")).click();
-            console.log(`Step 13 -> Completed game ${gamesToMade + 1}`);
+		while (gamesToMade < HOW_MUCH_GAMES_TO_PLAY) {
+			await driver.findElement(By.id("completeojogo")).click();
+			console.log(`Step 13 -> Completed game ${gamesToMade + 1}`);
 
-            await driver.sleep(2000);
+			await driver.sleep(2000);
 
-            await driver.findElement(By.id("colocarnocarrinho")).click();
-            gamesToMade++;
-            await driver.sleep(2000);
-        }
+			await driver.findElement(By.id("colocarnocarrinho")).click();
+			gamesToMade++;
+			await driver.sleep(2000);
+		}
 
-        await driver.findElement(By.id("irparapagamento")).click();
-        console.log("Step 14 -> Clicked 'Ir para pagamento'");
+		await driver.findElement(By.id("irparapagamento")).click();
+		console.log("Step 14 -> Clicked 'Ir para pagamento'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("confirma")).click();
-        console.log("Step 15 -> Clicked 'Confirmar'");
+		await driver.findElement(By.id("confirma")).click();
+		console.log("Step 15 -> Clicked 'Confirmar'");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.css("span img[alt='Pix']")).click();
-        console.log("Step 16 -> Selected Pix as payment method");
+		await driver.findElement(By.css("span img[alt='Pix']")).click();
+		console.log("Step 16 -> Selected Pix as payment method");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.css("input[type='checkbox'][ng-model='vm.confirmacao']")).click();
-        console.log("Step 17 -> Confirmed Pix payment");
+		await driver.findElement(By.css("input[type='checkbox'][ng-model='vm.confirmacao']")).click();
+		console.log("Step 17 -> Confirmed Pix payment");
 
-        await driver.sleep(2000);
+		await driver.sleep(2000);
 
-        await driver.findElement(By.id("payPix")).click();
-        console.log("Step 18 -> Clicked 'Pagar Agora'");
+		await driver.findElement(By.id("payPix")).click();
+		console.log("Step 18 -> Clicked 'Pagar Agora'");
 
-        const valorTotalApostas = await driver.findElement(By.id("valortotalapostas")).getText();
-        console.log(`Step 19 -> Total value: ${valorTotalApostas}`);
+		const valorTotalApostas = await driver.findElement(By.id("valortotalapostas")).getText();
+		console.log(`Step 19 -> Total value: ${valorTotalApostas}`);
 
-        const concursos = await driver.findElement(By.css("p.ng-binding")).getText();
-        console.log(`Step 20 -> Contest details: ${concursos}`);
+		const concursos = await driver.findElement(By.css("p.ng-binding")).getText();
+		console.log(`Step 20 -> Contest details: ${concursos}`);
 
-        console.log("Step 21 -> Collecting game numbers...");
+		console.log("Step 21 -> Collecting game numbers...");
 
-        let games = [];
-        let [game1, game2, game3, game4, game5, game6, game7] = [[], [], [], [], [], [], []];
-        for (let i = 1; i <= HOW_MUCH_GAMES_TO_PLAY; i++) {
-            const gameNumbers = [];
+		let games = [];
+		let [game1, game2, game3, game4, game5, game6, game7] = [[], [], [], [], [], [], []];
+		for (let i = 1; i <= HOW_MUCH_GAMES_TO_PLAY; i++) {
+			const gameNumbers = [];
 
-            const elements = await driver.findElements(By.css(`#game${i} span.margemVolante`));
+			const elements = await driver.findElements(By.css(`#game${i} span.margemVolante`));
 
-            for (const el of elements) {
-                const numberText = await el.getText();
-                const number = parseInt(numberText, 10);
+			for (const el of elements) {
+				const numberText = await el.getText();
+				const number = parseInt(numberText, 10);
 
-                if (!isNaN(number)) {
-                    gameNumbers.push(number);
-                }
-            }
+				if (!isNaN(number)) {
+					gameNumbers.push(number);
+				}
+			}
 
-            games.push(gameNumbers);
-        }
+			games.push(gameNumbers);
+		}
 
-        console.log("Games:", games);
+		console.log("Games:", games);
 
-        // Passo 22 -> Salvando dados no banco de dados JSON
-        await driver.wait(until.elementLocated(By.id("codigoPix")), 10000);
-        const codigoPixValue = (await driver.findElement(By.id("codigoPix")).getAttribute("value")).replace(/\s+/g, "");
+		// Passo 22 -> Salvando dados no banco de dados JSON
+		await driver.wait(until.elementLocated(By.id("codigoPix")), 10000);
+		const codigoPixValue = (await driver.findElement(By.id("codigoPix")).getAttribute("value")).replace(/\s+/g, "");
 
-        [game1, game2, game3, game4, game5, game6, game7] = games;
-        console.log(`Passo 22 -> Salvando dados no banco de dados JSON...`);
+		[game1, game2, game3, game4, game5, game6, game7] = games;
+		console.log(`Passo 22 -> Salvando dados no banco de dados JSON...`);
 
-        const currentData = readJsonFile("./src/repositories/jsons/played.json");
+		const currentData = readJsonFile("./src/repositories/jsons/played.json");
 
-        const newObject = {
-            id: randomUUID(),
-            created_at: DateTime.getNow(),
-            concurso: concursos.replace("Concurso: ", ""),
-            pix: codigoPixValue,
-            total: valorTotalApostas,
-            played: games,
-        };
+		const newObject = {
+			id: randomUUID(),
+			created_at: DateTime.getNow(),
+			concurso: concursos.replace("Concurso: ", ""),
+			pix: codigoPixValue,
+			total: valorTotalApostas,
+			played: games,
+		};
 
-        currentData.push(newObject);
-        writeJsonFile("./src/repositories/jsons/played.json", currentData);
+		currentData.push(newObject);
+		writeJsonFile("./src/repositories/jsons/played.json", currentData);
 
-        TelegramLog.info(
-            `\n\nPagar Novo Jogo Lotomania!\n\nPIX: ${codigoPixValue}\n\nValor total das apostas: ${valorTotalApostas}\n\n${concursos}\n\nNúmeros dos jogos:\n\n` +
-            `JOGO 1\n[${game1.join(", ")}]\n\n` +
-            `JOGO 2\n[${game2.join(", ")}]\n\n` +
-            `JOGO 3\n[${game3.join(", ")}]\n\n` +
-            `JOGO 4\n[${game4.join(", ")}]\n\n` +
-            `JOGO 5\n[${game5.join(", ")}]\n\n` +
-            `JOGO 6\n[${game6.join(", ")}]\n\n` +
-            `JOGO 7\n[${game7.join(", ")}]\n\n`,
-        );
+		TelegramLog.info(
+			`\n\nPagar Novo Jogo Lotomania!\n\nPIX: ${codigoPixValue}\n\nValor total das apostas: ${valorTotalApostas}\n\n${concursos}\n\nNúmeros dos jogos:\n\n` +
+				`JOGO 1\n[${game1.join(", ")}]\n\n` +
+				`JOGO 2\n[${game2.join(", ")}]\n\n` +
+				`JOGO 3\n[${game3.join(", ")}]\n\n` +
+				`JOGO 4\n[${game4.join(", ")}]\n\n` +
+				`JOGO 5\n[${game5.join(", ")}]\n\n` +
+				`JOGO 6\n[${game6.join(", ")}]\n\n` +
+				`JOGO 7\n[${game7.join(", ")}]\n\n`,
+		);
 
-        console.log(`Passo 23 -> Copiou valor codigoPixValue e enviou dados para o Telegram!!!`);
-    } catch (error: any) {
-        console.log("Error encountered:", error.message);
-    } finally {
-        // await driver.quit();
-    }
+		console.log(`Passo 23 -> Copiou valor codigoPixValue e enviou dados para o Telegram!!!`);
+	} catch (error: any) {
+		console.log("Error encountered:", error.message);
+	} finally {
+		// await driver.quit();
+	}
 
-    console.log(`\n\nBOT Loteria Galhardo Finalizado!\n\n`);
+	console.log(`\n\nBOT Loteria Galhardo Finalizado!\n\n`);
 }
